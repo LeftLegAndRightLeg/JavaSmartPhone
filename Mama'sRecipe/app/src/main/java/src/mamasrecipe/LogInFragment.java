7e597056f58cd54b089561d3c2234c17903a30dd
巩柏良 <gongbailiang@gongbolangdeMBP.sv.cmu.local>
2015-07-28 16:37:15 -0700

    Add more Activities

---
+++ Mama'sRecipe/app/src/main/java/src/mamasrecipe/LogInFragment.java
@@ -0,0 +1,23 @@
+package src.mamasrecipe;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class LogInFragment extends Fragment {
+
+    public LogInFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_log_in, container, false);
+    }
+}
