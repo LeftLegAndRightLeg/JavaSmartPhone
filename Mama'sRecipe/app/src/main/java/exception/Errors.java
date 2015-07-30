397b33e864e7de2025300ed120ba24130528f6b7
Na Li <Na@Nas-MacBook-Pro.sv.cmu.local>
2015-07-29 17:15:53 -0700

    Na's Version. Adding more Activities

---
+++ Mama'sRecipe/app/src/main/java/exception/Errors.java
@@ -0,0 +1,25 @@
+package exception;
+
+/**
+ * Created by Na on 7/29/15.
+ */
+public enum Errors {
+
+    NO1(1, "User not found!"),
+    NO2(2, "Image not found");
+
+    private final int errorno;
+    private final String errormsg;
+
+    private Errors(int errorno, String errormsg){
+        this.errorno = errorno;
+        this.errormsg = errormsg;
+    }
+
+    public int getErrorno(){
+        return errorno;
+    }
+    public String getErrormsg(){
+        return errormsg;
+    }
+}
---
+++ Mama'sRecipe/app/src/main/java/exception/Fixer.java
@@ -0,0 +1,14 @@
+package exception;
+
+/**
+ * Created by Na on 7/29/15.
+ */
+public class Fixer {
+    public void fix1(){
+
+    }
+
+    public void fix2(){
+
+    }
+}
