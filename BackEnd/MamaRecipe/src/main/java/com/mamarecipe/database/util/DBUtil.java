9826264edb7e23fd7fc537eddbc2881f5c190c2d
Jeremiah Lin <cc.jeremiah.lin@gmail.com>
2015-08-12 11:34:35 -0700

    recover

---
+++ BackEnd/MamaRecipe/src/main/java/com/mamarecipe/database/util/DBUtil.java
@@ -0,0 +1,21 @@
+package com.mamarecipe.database.util;		
+		
+import java.sql.Connection;		
+import java.sql.DriverManager;		
+import java.sql.SQLException;		
+		
+/**	
+ * Created by Jeremiah on 7/26/15.		
+ */		
+public class DBUtil {		
+    private DBUtil() {}		
+    public static final Connection getConnection() throws SQLException, ClassNotFoundException {		
+        System.setProperty("jdbc.drivers", "com.mysql.jdbc.Driver");		
+        String url = "jdbc:mysql://jdbcjavasp.cq6tvjnzkrnl.us-west-1.rds.amazonaws.com:3306";		
+        String username = "BobIsAwesome";		
+        String password = "YesHeIsAwesome";		
+        String db = "mamarecipe";		
+        Class.forName("com.mysql.jdbc.Driver");		
+        return DriverManager.getConnection(url + "/" + db, username, password);		
+    }		
+}
